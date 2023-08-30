c5b7f2933d5c2a3f130fee2540f9497dee3b9556
Samuel C. B <141483244+SamCme@users.noreply.github.com>
2023-08-30 13:41:40 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_fleur.mk
+    $(LOCAL_DIR)/leaf_fleur.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_fleur-user \
-    lineage_fleur-userdebug \
-    lineage_fleur-eng
+    leaf_fleur-user \
+    leaf_fleur-userdebug \
+    leaf_fleur-eng
